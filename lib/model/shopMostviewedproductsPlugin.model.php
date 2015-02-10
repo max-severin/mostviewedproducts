7bdfef6a9b497fec70519fb1006696a7037e21ec
Max Severin <makc.severin@gmail.com>
2015-02-10 13:43:26 +0300

    added save method in shopMostviewedproductsPluginModel

--- lib/model/shopMostviewedproductsPlugin.model.php
+++ lib/model/shopMostviewedproductsPlugin.model.php
@@ -4,7 +4,32 @@
  * @author Max Severin <makc.severin@gmail.com>
  */
 
-class shopMostviewedproductsPluginModel extends waModel
-{
+class shopMostviewedproductsPluginModel extends waModel {
+
     protected $table = 'shop_mvp_product_views';
+
+    public function save($product) {
+
+    	if ($old_data = $this->getByField('product_id', $product['id'])) {
+
+    		$fields = array(
+			    'product_id' => $product['id'],
+			);
+			$data = array(
+			    'view' => (int)$old_data['view'] + 1,
+			);
+			$this->updateByField($fields, $data);
+
+    	} else {
+
+    		$data = array(
+			    'product_id' => $product['id'],
+			    'view' => 1,
+			);
+			$this->insert($data, 1);
+			
+    	}
+
+    }
+
 }
