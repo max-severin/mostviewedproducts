c4c961de12700e55922824de693ff6af928ddfaf
Max Severin <makc.severin@gmail.com>
2015-06-24 14:41:54 +0300

    plugin was localised

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,8 +4,8 @@
  * @author Max Severin <makc.severin@gmail.com>
  */
 return array(
-    'name' => ('Самые просматриваемые товары'),
-    'description' => ('Учет просмотров товаров и возможность вывода списка самых популярных'),
+    'name' => /*_wp*/('Most viewed products'),
+    'description' => /*_wp*/('Accounting views of products and the ability to output the list of the most popular'),
     'version'=>'1.0.0',
     'img'=>'img/mostviewedproducts.png',
     'handlers' => array(
